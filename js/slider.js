30de2aa92bf93c4bb892906cb154e4dcc081f084
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-02-27 13:53:51 +0100

    Sliders er nu med dots og med udkommenteret tidsfunktion

--- js/slider.js
+++ js/slider.js
@@ -1,22 +1,26 @@
 //DOM elementer
 
 let slides = document.querySelectorAll(".slide")
-
+let dots = document.querySelectorAll(".dot")
 
 //Counter der holder styr på hvilket billede vi er nået til
-
 let slideIndex = 1
 
 //Events der kalder vores funktion
 //Timer
-window.setInterval(function () {
-    showSlides(slideIndex += 1)
-}, 4000)
+//window.setInterval(function () {
+//    showSlides(slideIndex += 1)
+//}, 4000)
 
-//onClick funktion
+//onClick funktion for prev/ next
 function plusSlides(number) {
     showSlides(slideIndex += number)
 
+}
+
+//onclick for dots
+function currentSlide(number) {
+    showSlides(slideIndex = number)
 }
 
 //Funktion der skifter slides
@@ -25,6 +29,7 @@ function showSlides(num) {
     if (num > slides.length) {
         slideIndex = 1
     }
+
     //Num må heller ikke være mindre end 1
     if (num < 1) {
         slideIndex = slides.length
@@ -32,6 +37,8 @@ function showSlides(num) {
 
     slides.forEach(function (element, index) {
         element.style.display = "none"
+        dots[index].classList.remove("slide-active")
         slides[slideIndex - 1].style.display = "block"
+        dots[slideIndex - 1].classList.add("slide-active")
     })
 }
