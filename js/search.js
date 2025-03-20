e290a2e2a9612df31e4052a89848074c3f0bcde9
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-03-20 13:39:58 +0100

    Søgefunktionen er blevet opdateret, så den fungere bedre nu.

--- js/search.js
+++ js/search.js
@@ -1,11 +1,10 @@
 let query = [
-    "arne",
-    "socialpædagog",
+    "christopher",
+    "GF2 Webudvikler studerende",
     "kontakt",
     "foto",
     "flex"
 ]
-
 let urls = [
     ["index.html", "foto.html", "kontakt.html"],
     ["index.html"],
@@ -13,25 +12,22 @@ let urls = [
     ["foto.html"],
     ["index.html", "foto.html", "kontakt.html"]
 ]
-
 let url = window.location.href
 url = new URL(url)
 let searchString = url.searchParams.get("search")
 let results = false;
 let htmlOutput = "Din søgning på <i>" + searchString + "</i> gav ingen resultater"
+
 query.forEach(function (element, index) {
     if (results == false) {
-
         if (searchString.toLowerCase() == element) {
-            htmlOutput = "Din søgning på <i>" + searchString + "</i> gav flg. resultater <br>"
+            htmlOutput = "Din søgning på  <i>" + searchString + "</i> gav flg. resultater <br>"
             urls[index].forEach(function (elm) {
                 htmlOutput += "<a href='" + elm + "'>" + elm + "</><br>"
             })
-            document.getElementById("search-results").innerHTML = htmlOutput
+
             results = true
-
         }
-
     }
 
 })
